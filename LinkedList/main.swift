b4e0e25b69343f9063add8b4c763ef05d9c58db1
yanghojoon <yanghojoon8487@gmail.com>
2021-12-21 21:02:16 +0900

    refactor: LinkedList 내부에 Node 타입 이동
    - Node의 경우 LinkedList 내에서만 사용하는 타입으로 내부에 구현

--- LinkedList/main.swift
+++ LinkedList/main.swift
@@ -18,6 +18,17 @@ class Node<Type> {
 }
 
 struct LinkedList<Type> {
+    
+    // Node는 next의 주소값이 필요하기 때문에 구조체가 아닌 클래스로 구현
+    class Node<Type> {
+        var data: Type
+        var next: Node?
+        
+        init(data: Type) {
+            self.data = data
+        }
+    }
+    
     var head: Node<Type>?
     var tail: Node<Type>?
     
