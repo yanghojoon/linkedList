f84edc5f6d0ecb0cec9e8563d9923d2e887a3079
yanghojoon <yanghojoon8487@gmail.com>
2022-01-12 10:25:33 +0900

    feat: isEmpty, first, last 프로퍼티 추가

--- LinkedList/LinkedList.swift
+++ LinkedList/LinkedList.swift
@@ -16,3 +16,24 @@ class LinkedListNode<T> {
         self.value = value
     }
 }
+
+class DoublyLinkedList<T> {
+    typealias Node = LinkedListNode<T>
+    
+    private var head: Node?
+    var isEmpty: Bool {
+        return head == nil
+    }
+    var first: Node? {
+        return head
+    }
+    // tail을 만들었다면 last는 바로 return tail을 해주면 됨. 
+    var last: Node? {
+        guard var node = head else { return }
+        
+        while let next = node.next {
+            node = next
+        }
+        return node
+    }
+}
